be883e24f2ac36b9f67ee054dad55f923a2edd5c
David LeFebvre <david@lefebvre.us>
2021-05-03 18:29:48 -0400

    started ShiftUpdate Vue component

--- vue/tiplife/src/App.vue
+++ vue/tiplife/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ShiftUpdate />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ShiftUpdate from '@/components/ShiftUpdate.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ShiftUpdate
   }
 }
 </script>
---
+++ vue/tiplife/src/components/ShiftUpdate.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="shift">
+        <form action="">
+            <label for="date">Date</label>
+            <input type="date" name="date" id="date">
+            <br>
+            <label for="daypart">Shift</label>
+            <select name="daypart" id="daypart">
+                <option value="lunch">Lunch</option>
+                <option value="dinner">Dinner</option>
+            </select>
+            <br>
+            <label for="tips">Tips</label>
+            <input type="text" name="tips" id="tips">
+            <br>
+            <label for="tipouts">Tipouts</label>
+            <input type="text" name="tipouts" id="tipouts">
+            <br>
+            <label for="hours">Hours</label>
+            <input type="number" name="hours" id="hours" step="0.01">
+            <br>
+            <label for="notes">Notes</label>
+            <textarea name="notes" id="notes" cols="30" rows="10"></textarea>
+            <br>
+            <button>Save</button>
+            <button>Cancel</button>
+        </form>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Shift",
+    
+}
+</script>
+
+<style>
+
+</style>
